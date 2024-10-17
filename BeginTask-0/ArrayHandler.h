637df05976faaaadd552c5c90cdf07f28023550f
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2024-10-18 02:10:33 +0300

    Array handler

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -2,28 +2,25 @@
 #include <algorithm>
 #include <cstring>
 #include <limits>
+#include <vector>
 template<typename T>
 class ArrayHandler {
 private:
     size_t _size;
-    T* _array;
+    std::vector<T> _array;
     size_t _count;
     T Min;
     T Max;
 public:
     ArrayHandler(size_t size = 10) {
         _size = size;
-        _array = (T*)malloc(size);
         _count = 0;
         Max = std::numeric_limits<T>::min();
         Min = std::numeric_limits<T>::max();
     }
 
     void AppendElem(T elem) {
-       if (_count == _size){
-            _array = (T*) realloc(_array, _size + sizeof(T));
-        }
-        _array[_count] = elem;
+       _array.push_back(elem);
         _count++;
         if(elem > Max)
             Max = elem;
@@ -32,7 +29,10 @@ public:
     }
 
     bool IsContains(T elem) {
-        return std::find(_array, _array+_count, elem)!=(_array +_count);
+        for(int i = 0; i < _count; i++){
+            if(elem == _array[i]) return true;
+        }
+        return false;
     }
 
     T GetMax() {
@@ -44,7 +44,7 @@ public:
     }
 
     ~ArrayHandler() {
-        free(_array);
+        _array.clear();
     }
 
 };
